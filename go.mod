f8f81a10b6de866b18093c1de2a8c8dd9d653b4d
reus <reusee@gmail.com>
2020-03-25 11:20:38 +0800

    add honnef.co/go/tools/staticcheck

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/reusee/pa
 
 go 1.14
 
-require golang.org/x/tools v0.0.0-20200324053659-5c746ccfa245
+require (
+	golang.org/x/tools v0.0.0-20200324053659-5c746ccfa245
+	honnef.co/go/tools v0.0.1-2020.1.3
+)
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "golang.org/x/tools/go/analysis/multichecker"
+import (
+	"golang.org/x/tools/go/analysis/multichecker"
+	"honnef.co/go/tools/staticcheck"
+)
 
 func main() {
+	for _, analyzer := range staticcheck.Analyzers {
+		Analyzers = append(Analyzers, analyzer)
+	}
 	multichecker.Main(Analyzers...)
 }

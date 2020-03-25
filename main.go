d1d6de42d150820886693a35c76cfbf547a67485
reus <reusee@gmail.com>
2020-03-25 11:42:57 +0800

    add honnef.co/go/tools/simple

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"golang.org/x/tools/go/analysis/multichecker"
+	"honnef.co/go/tools/simple"
 	"honnef.co/go/tools/staticcheck"
 )
 
 func main() {
 	for _, analyzer := range staticcheck.Analyzers {
+		Analyzers = append(Analyzers, analyzer)
+	}
+	for _, analyzer := range simple.Analyzers {
 		Analyzers = append(Analyzers, analyzer)
 	}
 	multichecker.Main(Analyzers...)
